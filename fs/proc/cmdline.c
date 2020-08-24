a447d2cb0e826e5ccb6a7d7358a5e8637ce15455
Josh Wiles <joshwiles@me.com>
2020-08-24 10:52:40 -0500

    Revert "Experimental safetynet patch"
    This reverts commit 0602dce1ef4f0d4a4051e33ae0730b415214fbb1.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -1,58 +1,29 @@
- #include <linux/init.h>
- #include <linux/proc_fs.h>
- #include <linux/seq_file.h>
- #include <asm/setup.h>
-
-static char new_command_line[COMMAND_LINE_SIZE];
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
-
 {
-	seq_printf(m, "%s\n", new_command_line);
- 	return 0;
- }
+	seq_printf(m, "%s\n", saved_command_line);
+	return 0;
+}
+
+static int cmdline_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, cmdline_proc_show, NULL);
+}
 
 static const struct file_operations cmdline_proc_fops = {
- 	.release	= single_release,
+	.open		= cmdline_proc_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
 };
-
-static void remove_flag(char *cmd, const char *flag)
-{
-	char *start_addr, *end_addr;
-	/* Ensure all instances of a flag are removed */
-	while ((start_addr = strstr(cmd, flag))) {
-		end_addr = strchr(start_addr, ' ');
-		if (end_addr)
-			memmove(start_addr, end_addr  1, strlen(end_addr));
-		else
-			*(start_addr - 1) = '\0';
-	}
-}
-
-
-
-static void remove_safetynet_flags(char *cmd)
-
-{
-	remove_flag(cmd, "androidboot.enable_dm_verity=");
-	remove_flag(cmd, "androidboot.secboot=");
-	remove_flag(cmd, "androidboot.verifiedbootstate=");
-	remove_flag(cmd, "androidboot.veritymode=");
-}
-
-
 
 static int __init proc_cmdline_init(void)
 {
- 	char *offset_addr;
-
-	strcpy(new_command_line, saved_command_line);
-
-	offset_addr = strstr(new_command_line, "androidboot.mode=reboot");
- 	if (offset_addr != NULL)
- 		strncpy(offset_addr  17, "normal", 6);
-
-	remove_safetynet_flags(new_command_line);
- 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
- 	return 0;
- }
+	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+	return 0;
+}
+fs_initcall(proc_cmdline_init);
